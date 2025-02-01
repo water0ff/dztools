392c96b005996177bd1b7ef07fef739d16dbbb39
water0ff <159820255+water0ff@users.noreply.github.com>
2025-02-01 14:47:03 -0600

    Update tools.ps1

--- tools.ps1
+++ tools.ps1
@@ -17,7 +17,7 @@ $formPrincipal.MinimizeBox = $false
 $defaultFont = New-Object System.Drawing.Font("Segoe UI", 10, [System.Drawing.FontStyle]::Regular)
 $boldFont = New-Object System.Drawing.Font("Segoe UI", 10, [System.Drawing.FontStyle]::Bold)
 # Crear un TextBox para ingresar la versión manualmente
-                                                                $version = "Alfa 250201.2329"  # Valor predeterminado para la versión
+                                                                $version = "Alfa 250201.2335"  # Valor predeterminado para la versión
 $formPrincipal.Text = "Daniel Tools v$version"
 Write-Host "`n=============================================" -ForegroundColor DarkCyan
 Write-Host "       Daniel Tools - Suite de Utilidades       " -ForegroundColor Green
@@ -37,7 +37,7 @@ function Create-Button {
     # Pasar esto a los parametros de arriba.
     $buttonStyle = @{
         Size      = New-Object System.Drawing.Size(220, 35)
-        FlatStyle = [System.Windows.Forms.FlatStyle]::Flat
+        FlatStyle = [System.Windows.Forms.FlatStyle]::System
         Font      = $defaultFont
     }
 
